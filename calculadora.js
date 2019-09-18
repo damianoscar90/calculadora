256a9e5adc643c0ae8714600d002cdc8c9af457c
oscar tarbuch <otarbuch@cpau.sis>
2019-09-18 20:35:43 -0300

    First commit

---
+++ calculadora.js
@@ -0,0 +1,22 @@
+function sumar() {
+
+var numero1 = document.getElementById("numero1").value //El .value es para sacar solo el valor
+var numero2 = document.getElementById("numero2").value 
+var resultado = 0;
+var camporesultado = document.getElementById("resultado") //Cuando el getelementbyid no es .value es para sacar todo el campo
+
+resultado = parseInt(numero1) +  parseInt(numero2)
+
+camporesultado.value = resultado
+}
+
+function restar(){
+    var numero1 = document.getElementById("numero1").value
+    var numero2 = document.getElementById("numero2").value 
+    var resultado = 0;
+    
+    
+    resultado = parseInt(numero1) -  parseInt(numero2)
+
+}
+
